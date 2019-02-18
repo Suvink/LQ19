a2d5c88746850054512bc534baf1f98a534e7aa6
suvink <suvink456@yahoo.com>
2019-02-18 20:01:22 +0530

    Linked with JSON

--- lib/livescore.dart
+++ lib/livescore.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:shimmer/shimmer.dart';
 import 'highlights.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 
 class livescores extends StatefulWidget {
   @override
@@ -9,9 +12,72 @@ class livescores extends StatefulWidget {
 }
 
 class _LivescoreState extends State<livescores> {
+
+  final String url = "https://api.myjson.com/bins/173mx6";
+  var jsonCrest;
+  var jsonScore;
+  var jsonOvers;
+  var jsonState;
+  var jsonStatus;
+  var jsonBatsmanOne;
+  var jsonBatsmanTwo;
+  var jsonBowler;
+  var jsonRunrate;
+  var jsonBattingTeam;
+  var jsonTotalExtras;
+  var jsonNoBalls;
+  var jsonWides;
+  var jsonLegByes;
+  var jsonBatsmanOneScore;
+  var jsonBatsmanTwoScore;
+  var jsonBowlerScore;
+
+
   @override
   void initState() {
     super.initState();
+    this.getJsonData();
+  }
+
+  Future<String> getJsonData() async {
+    http.Response response = await http.get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
+
+    var data = jsonDecode(response.body);
+
+    setState(() {
+      //jsonState = data['setState'];
+      jsonStatus = data['string'];
+      jsonScore = data['score'];
+      jsonOvers =data['overs'].toString() + " Overs";
+      jsonBattingTeam = data['battingTeam'];
+
+      if (jsonBattingTeam == "RICHMOND"){
+        jsonCrest = "assets/crest_rcg.png";
+      } else{
+        jsonCrest = "assets/crest_mcg.png";
+      }
+
+      //Batsman and Bowlers details
+      jsonBatsmanOne = data['batsmanOne']['name'].toString()  + "*" ;
+      jsonBatsmanOneScore = data['batsmanOne']['runs'].toString();
+      jsonBatsmanTwo = data['batsmanTwo']['name'].toString() ;
+      jsonBatsmanTwoScore = data['batsmanTwo']['runs'].toString();
+      jsonBowler = data['bowlerOne']['name'].toString();
+      jsonBowlerScore = data['bowlerOne']['figures'].toString();
+
+
+      jsonRunrate = data['runRate'];
+      jsonTotalExtras = data['extras']['totalExtras'].toString() + "\nTotal Extras";
+      jsonNoBalls = data['extras']['noBalls'].toString() + "\nNo Balls";
+      jsonWides = data['extras']['wides'].toString() + "\nWides";
+      jsonLegByes = data['extras']['legByes'].toString() + "\nLeg Byes";
+
+
+    });
+
+    print(jsonState.toString());
+    print(jsonStatus.toString());
+
   }
 
   @override
@@ -39,7 +105,7 @@ class _LivescoreState extends State<livescores> {
     //Logo
     final crest = new Center(
       child: Image(
-        image: AssetImage("./assets/crest_rcg.png"),
+        image: AssetImage(jsonCrest.toString()),
         height: 79.0,
         width: 64.0,
       ),
@@ -51,7 +117,7 @@ class _LivescoreState extends State<livescores> {
       padding: EdgeInsets.only(bottom: 0.0),
       alignment: Alignment(0.0, 0.0),
       child:
-          Text("144/4", style: TextStyle(fontSize: 72.0, color: Colors.white)),
+          Text(jsonScore.toString(), style: TextStyle(fontSize: 72.0, color: Colors.white)),
     );
 
     //Overs count
@@ -59,7 +125,7 @@ class _LivescoreState extends State<livescores> {
       padding: EdgeInsets.only(bottom: 0.0),
       margin: EdgeInsets.only(top: 0.0),
       alignment: Alignment(0.0, 0.0),
-      child: Text("32.2 Overs",
+      child: Text(jsonOvers.toString(),
           style: TextStyle(fontSize: 24.0, color: Color(0xFFB4B4B4))),
     );
 
@@ -146,25 +212,25 @@ class _LivescoreState extends State<livescores> {
         shrinkWrap: true,
         children: <Widget>[
           Text(
-            "4\nTotal Extras",
+            jsonTotalExtras.toString(),
             style: TextStyle(fontSize: 14.0, color: Colors.white),
             textAlign: TextAlign.center,
           ),
           SizedBox(width: 15.0),
           Text(
-            "2\n Wides",
+            jsonWides.toString(),
             style: TextStyle(fontSize: 14.0, color: Colors.white),
             textAlign: TextAlign.center,
           ),
           SizedBox(width: 15.0),
           Text(
-            "0\n No Balls",
+            jsonNoBalls.toString(),
             style: TextStyle(fontSize: 14.0, color: Colors.white),
             textAlign: TextAlign.center,
           ),
           SizedBox(width: 15.0),
           Text(
-            "1\n Leg Bys",
+            jsonLegByes.toString(),
             style: TextStyle(fontSize: 14.0, color: Colors.white),
             textAlign: TextAlign.center,
           )
@@ -179,7 +245,7 @@ class _LivescoreState extends State<livescores> {
         children: <Widget>[
           new Container(
             child: Image(
-              image: AssetImage("./assets/Bowler.png"),
+              image: AssetImage("./assets/batsman.png"),
               height: 94.0,
               width: 53.0,
             ),
@@ -187,7 +253,7 @@ class _LivescoreState extends State<livescores> {
           new Container(
               child: new Shimmer.fromColors(
                   child: Text(
-                    "Bowling",
+                    "Batting",
                     style: TextStyle(fontSize: 24.0),
                   ),
                   baseColor: Colors.white,
@@ -196,7 +262,7 @@ class _LivescoreState extends State<livescores> {
           new Container(
               child: Shimmer(
             child: Image(
-              image: AssetImage("./assets/batsman.png"),
+              image: AssetImage("./assets/Bowler.png"),
               height: 94.0,
               width: 53.0,
             ),
@@ -210,7 +276,7 @@ class _LivescoreState extends State<livescores> {
           new Container(
               child: new Shimmer.fromColors(
                   child: Text(
-                    "Batting",
+                    "Bowling",
                     style: TextStyle(fontSize: 24.0),
                   ),
                   baseColor: Colors.white,
@@ -219,60 +285,60 @@ class _LivescoreState extends State<livescores> {
       ),
     );
 
-    final bowlerDetails = new Container(
+    final batsmanDetails = new Container(
       margin: EdgeInsets.only(left: 10.0, right: 30.0),
       child: Column(
         children: <Widget>[
           new Container(
               child: Text(
-            "Charith Asalanka*",
-            style: TextStyle(fontSize: 14.0, color: Colors.white),
+            jsonBatsmanOne.toString(),
+            style: TextStyle(fontSize: 12.0, color: Colors.white),
           )),
           new Container(
               margin: EdgeInsets.only(bottom: 10.0),
               child: Text(
-                "53(12)",
-                style: TextStyle(fontSize: 14.0, color: Colors.white),
+                jsonBatsmanOneScore.toString(),
+                style: TextStyle(fontSize: 12.0, color: Colors.white),
               )),
           new Container(
               child: Text(
-            "Charith Asalanka",
-            style: TextStyle(fontSize: 14.0, color: Colors.white),
+            jsonBatsmanTwo.toString(),
+            style: TextStyle(fontSize: 12.0, color: Colors.white),
           )),
           new Container(
               child: Text(
-            "53(12)",
-            style: TextStyle(fontSize: 14.0, color: Colors.white),
+            jsonBatsmanTwoScore.toString(),
+            style: TextStyle(fontSize: 12.0, color: Colors.white),
           )),
         ],
       ),
     );
 
-    final batsmanDetails = new Container(
+    final bowlerDetails = new Container(
       alignment: Alignment(1.0, 0.0),
-      margin: EdgeInsets.only(left: 30.0, right: 10.0),
+      margin: EdgeInsets.only(left: 15.0, right: 10.0),
       child: Column(
         children: <Widget>[
           new Container(
               child: Text(
-            "Charith Asalanka*",
-            style: TextStyle(fontSize: 14.0, color: Colors.white),
+            jsonBowler.toString(),
+            style: TextStyle(fontSize: 12.0, color: Colors.white),
           )),
           new Container(
               margin: EdgeInsets.only(bottom: 10.0),
               child: Text(
-                "53(12)",
-                style: TextStyle(fontSize: 14.0, color: Colors.white),
+                jsonBowlerScore.toString(),
+                style: TextStyle(fontSize: 12.0, color: Colors.white),
               )),
           new Container(
               child: Text(
-            "Charith Asalanka",
-            style: TextStyle(fontSize: 14.0, color: Colors.white),
+            "Run Rate",
+            style: TextStyle(fontSize: 12.0, color: Colors.white),
           )),
           new Container(
               child: Text(
-            "53(12)",
-            style: TextStyle(fontSize: 14.0, color: Colors.white),
+            jsonRunrate.toString(),
+            style: TextStyle(fontSize: 12.0, color: Colors.white),
           )),
         ],
       ),
@@ -281,7 +347,7 @@ class _LivescoreState extends State<livescores> {
     //Batsman and Bowler Wrapper
     final batsmanandbowler = new Row(
       mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[bowlerDetails, batsmanDetails],
+      children: <Widget>[batsmanDetails, bowlerDetails],
     );
 
     //Line separator
@@ -296,7 +362,7 @@ class _LivescoreState extends State<livescores> {
         margin: EdgeInsets.only(left: 10.0, right: 10.0),
         child: new Shimmer.fromColors(
             child: Text(
-              "Richmond College won the toss and elected to bat first",
+              jsonStatus.toString(),
               style: TextStyle(fontSize: 12.0),
             ),
             baseColor: Colors.white,
